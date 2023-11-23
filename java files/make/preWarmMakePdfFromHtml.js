4f401424a195fd5b81a341a70ecf14a3a4944df7
Fillebotten <fsandbacka@gmail.com>
2023-11-23 14:25:17 +0100

    deleted java files map with files we dont use

--- java files/make/preWarmMakePdfFromHtml.js
+++
@@ -1,9 +0,0 @@
-/***
- * - Starts Puppeteer early to have it ready when needed
- */
-
-export async function preWarmMakePDFFromHtml() {
-  let browser = await puppeteer.launch({ headless: 'new' });
-  let page = await browser.newPage();
-  return { browser, page };
-}
--- java files/make/reJustify.js
+++
@@ -1,7 +0,0 @@
-/***
- * - Helper for space width calculation during letter spacing
- */
-
-export function reJustify() {
-  document.querySelector('style.non-justify').remove();
-}
