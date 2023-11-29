1330bb6e3bf100d2644bca0758b1033a11f64df0
Richard Nykvist <77628633+MarcRick@users.noreply.github.com>
2023-11-29 11:38:30 +0100

    generated pic is background now /erik/Richard

--- js/buildAI.js
+++ js/buildAI.js
@@ -1,9 +1,8 @@
 
 async function postData() {
     const style = document.getElementById('styleSelect').value;
-    const promptI = document.getElementById('promptInput').value;
-    const prompt = promptI +", "+ style;
     const textValue = document.getElementById('editor');
+    const prompt = textValue.value +", "+ style;
 
 
     const optionsResponse = await fetch('http://127.0.0.1:5000/api/post_data', {
@@ -25,15 +24,36 @@ async function postData() {
         if (data.error) {
             console.error(data.error);
         } else {
+            const background = document.querySelector('.current-page');
             const imgElement = document.createElement('img');
+            const textArray = textValue.value.split('---').at(-1);
             imgElement.src = data.image_url;
-            localStorage.setItem(promptI, imgElement.src);
-            const newText = `![](${promptI})`;
-            textValue.value += newText;
+            localStorage.setItem(textArray, imgElement.src);
+            const pic = localStorage.getItem(textArray);
+            background.style.background = `url(${pic})`;
+
+            // const newText = `![](${promptI})`;
+            // textValue.value += newText;
             updatePreview();
 
         }
     } else {
         console.error('Preflight request failed');
     }
+}
+
+function showPic(){
+    const input = document.getElementById('editor');
+    const background = document.querySelector('.current-page');
+    const pic = localStorage.getItem(input.value);
+
+    const textArray = input.value.split('---').at(-1);
+
+    // console.log(input);
+    console.log(background);
+    // console.log(pic);
+    console.log(textArray);
+
+    // background.style.background = `url(${pic})`;
+
 }
